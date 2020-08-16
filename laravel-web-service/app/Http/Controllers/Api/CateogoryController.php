6bc0283bcbd0a9da318813c5e400d134302502a2
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-16 15:07:51 -0300

    Cadastrar Categoria API Laravel

--- laravel-web-service/app/Http/Controllers/Api/CateogoryController.php
+++ laravel-web-service/app/Http/Controllers/Api/CateogoryController.php
@@ -8,12 +8,31 @@ use App\Models\Category;
 
 class CateogoryController extends Controller
 {
+    
+    private $category;
+
+    public function __construct(Category $category)
+    {
+
+        $this->category = $category;
+        
+    }
+
+    
     public function index(Category $category, Request $request)
     {
 
-        $categories =  $category->getResults($request->name);
+        $categories =  $this->category->getResults($request->name);
 
         return response()->json($categories);
-        
+
     }
+
+    public function store(Request $request)
+    {
+        $category = $this->category->create($request->all());
+
+        return response()->json($category, 201);
+    }
+
 }
--- laravel-web-service/routes/api.php
+++ laravel-web-service/routes/api.php
@@ -1,25 +1,7 @@
 <?php
 
-//use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-//funciona
-//Route::get('categories','Api\CateogoryController@index');
-
 
 $this->get('categories','Api\CateogoryController@index');
+$this->post('categories','Api\CateogoryController@store');
 
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
