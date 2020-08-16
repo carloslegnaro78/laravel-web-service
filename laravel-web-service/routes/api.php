afe7510dd3acace497737f0029371ed9ac0bf7b3
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-16 15:48:53 -0300

    Editar Categoria API Laravel

--- laravel-web-service/routes/api.php
+++ laravel-web-service/routes/api.php
@@ -3,5 +3,6 @@
 
 $this->get('categories','Api\CateogoryController@index');
 $this->post('categories','Api\CateogoryController@store');
+$this->put('categories/{id}','Api\CateogoryController@update');
 
 
