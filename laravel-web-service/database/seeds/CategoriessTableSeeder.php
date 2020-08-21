44eabc34af60a8f38729bd7b82886462899c73e4
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-21 15:08:58 -0300

    DEFINIR RELACIONAMENTO TABELA PRODUTOS E CATEGORIAS

---
+++ laravel-web-service/database/seeds/CategoriessTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoriessTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name'  =>  'PHP',
+        ]);
+    }
+}
