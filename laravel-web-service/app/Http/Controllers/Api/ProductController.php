bd566c85f965f1c6273829180ead61052cfd82d7
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-23 20:42:30 -0300

    DELETAR PRODUTO API LARAVEL

--- laravel-web-service/app/Http/Controllers/Api/ProductController.php
+++ laravel-web-service/app/Http/Controllers/Api/ProductController.php
@@ -50,6 +50,13 @@ class ProductController extends Controller
 
     public function destroy($id)
     {
-        //
+        
+        if (!$product = $this->product->find($id))
+        return response()->json(['error' => 'Not Found'], 404);
+
+        $product->delete();
+
+        return response()->json(['sucesss' => true], 204);
+        
     }
 }
